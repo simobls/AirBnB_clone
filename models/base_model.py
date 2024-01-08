8a6015c358da7a0d71dc8e545acb6027c28f8385
simobls <belassalsimo78@gmail.com>
2024-01-08 21:57:54 +0100

    add

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,19 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-    
+
+    def save(self):
+        """Update the public instance attribute 'updated_at' with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the object."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
     def __str__(self):
         """Return a string representation of the object."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
-    def __repr__(self):
-        """Return a representation of the object."""
-        return self.__str__()
