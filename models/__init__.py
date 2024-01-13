9a81d2a60e69c87abdb137d37e42cf7fe7276671
simobls <belassalsimo78@gmail.com>
2024-01-13 19:52:43 +0100

    add

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,7 @@
 """
 Create a unique FileStorage instance for your application
 """
-from .engine.file_storage import FileStorage
-
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,54 +1,46 @@
-#!/usr/bin/python3
-"""
-Defines the base model
-"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """
-    Defines all common attributes and methods for other classes
-    Also links BaseModel to FileStorage by using the variable storage
-    """
     def __init__(self, *args, **kwargs):
-        """
-        Initializes an instance
-        """
-        if kwargs is not None and len(kwargs) != 0:
-            if '__class__' in kwargs:
-                del kwargs['__class__']
-            kwargs['created_at'] = datetime.fromisoformat(kwargs['created_at'])
-            kwargs['updated_at'] = datetime.fromisoformat(kwargs['updated_at'])
-            self.__dict__.update(kwargs)
-        else:
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            from .__init__ import storage
-            storage.new(self)
+            self.updated_at = self.created_at
+            storage.new(self)  # Add this line for new instances
+        else:
+            # Handle missing 'updated_at' or 'created_at'
+            kwargs['updated_at'] = datetime.strptime(kwargs.get('updated_at',
+                                                                datetime.now()
+                                                                ),
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs.get('created_at',
+                                                                datetime.now()
+                                                                ),
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            # Use pop() to safely remove '__class__'
+            kwargs.pop('__class__', None)
+
+            # Use update() to efficiently update multiple attributes
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """
-        String representation when instance is printed
-        """
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """
-        Save updates to an instance
-        """
-        self.__dict__.update({'updated_at': datetime.now()})
-        from .__init__ import storage
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """
-        Returns a dictionary representation of an instance
-        """
-        disdict = dict(self.__dict__)
-        disdict.update({'__class__': type(self).__name__,
-                        'updated_at': self.updated_at.isoformat(),
-                        'id': self.id,
-                        'created_at': self.created_at.isoformat()})
-        return disdict
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+    def reload(self):
+        storage.reload()
