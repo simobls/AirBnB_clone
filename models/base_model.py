65e808cff091b2ef9a8d7479cb688ee2ba21ff80
simobls <belassalsimo78@gmail.com>
2024-01-13 12:44:24 +0100

    add

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
-import datetime
+from datetime import datetime
 import uuid
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """Update the public instance attribute 'updated_at'
