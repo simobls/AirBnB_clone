07d32e006b79f62df5fa221f49970a9c1f394fa6
simobls <belassalsimo78@gmail.com>
2024-01-14 18:43:22 +0100

    add

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,47 @@
-#!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.updated_at = self.created_at
+            storage.new(self)  # Add this line for new instances
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            # Handle missing 'updated_at' or 'created_at'
+            kwargs['updated_at'] = datetime.strptime(kwargs.get('updated_at',
+                                                                datetime.now()
+                                                                ),
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            kwargs['created_at'] = datetime.strptime(kwargs.get('created_at',
+                                                                datetime.now()
+                                                                ),
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+
+            # Use pop() to safely remove '__class__'
+            kwargs.pop('__class__', None)
+
+            # Use update() to efficiently update multiple attributes
             self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict['id'] = obj_dict.get('id', None)
+        return obj_dict
+
+    def reload(self):
+        storage.reload()
